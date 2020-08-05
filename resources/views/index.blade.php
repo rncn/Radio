5b6838aab70495119e516698efb66ebbb75f09d2
rncn <doraidamon1rwfo@outlook.jp>
2020-08-05 11:54:35 +0900

    esign changed

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -7,11 +7,66 @@
   @endif
   <div class="home-wrapper">
     <div class="home-tl">
+      @if(! Auth::check())
+        <div class="alert-card">
+            <div class="form">
+              <div class="mainform">
+                <form method="POST" action="{{ route('login') }}">
+                  @csrf
+                  <div class="form-item">
+                      <label for="email">メールアドレス</label>
+                      <input id="email" type="email" class="@error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+    
+                          @error('email')
+                              <div class="invalid-feedback form-alert">
+                                  <strong>{{ $message }}</strong>
+                              </div>
+                          @enderror
+                  </div>
+                  <div class="form-item">
+                      <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+    
+                          <input id="password" type="password" class="@error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+    
+                          @error('password')
+                              <div class="invalid-feedback alert">
+                                  <strong>{{ $message }}</strong>
+                              </div>
+                          @enderror
+                  </div>
+                  <div class="form-item uk-align-right">    
+                              <label class="form-check-label">
+                                <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                                  {{ __('Remember Me') }}
+                              </label>
+                          <button type="submit" class="form-button">
+                              {{ __('Login') }}
+                          </button>
+                  </div>
+                </form>
+              </div> 
+              <div class="description">
+                  <h1>ログイン</h1>
+                  <h2>ログインして、機能を最大限使おう</h2>
+                  <a class="btn btn-link" href="{{ route('password.request') }}">
+                      {{ __('パスワードをお忘れですか？') }}
+                  </a>
+                  <a class="btn btn-link" href="{{ route('register') }}">
+                          {{ __('新規登録') }}
+                  </a>
+              </div>
+            </div>
+        </div>
+      @endif
       <div class="showcase" style="background-image: url(https://cdn.pixabay.com/photo/2020/07/03/06/12/people-5365324_1280.jpg);">
         <div class="content">
           <p>Recommended</p>
           <h2>testtesttestTestTestTestTestTestTestTest</h2>
         </div>
+      </div>
+      <div class="post">
+        <h2 class="post-title">ポストのタイトル~~~~</h2>
+        <p class="post-wrapper">ないようだなも<br/>「だなも」って本当は名古屋弁だよ！なのにどっかのたぬきが「だなも」「だなも」言うからどっかのたぬきのつかうことばみたいになってるよね。もうどうにかしてほしいだなも。でも、最近では名古屋でも「だなも」はつかわなくなったんだなも。<br/>＞ちな、名古屋圏では学校の授業の間の休み時間のことを「放課」って言ったりするね！わかるんだったら挙手だnaもだなも<br/>まじ🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺🥺だがね</p>
       </div>
     </div>
     <div class="home-nav">
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -7,25 +7,25 @@
           
               <div class="form-title">
                 <label for="title">タイトル</label> 
-                <input class="uk-input" name="title" value="{{ old('title') }}">
+                <input type="text" name="title" value="{{ old('title') }}">
               </div>
               @error('title')
-                  <div class="invalid-feedback uk-alert-danger" uk-alert>
+                  <div class="invalid-feedback form-alert">
                       <strong>{{ $message }}</strong>
                   </div>
               @enderror
               <div class="form-content">
                 <label for="content" class="form-content">内容</label> 
-                <textarea class="uk-input" name="content" cols="50" rows="10">{{ old('content') }}</textarea>        
+                <textarea class="form-button" name="content" cols="50" rows="10">{{ old('content') }}</textarea>        
               </div>
               @error('content')
-                  <div class="invalid-feedback uk-alert-danger" uk-alert>
+                  <div class="invalid-feedback form-alert">
                       <strong>{{ $message }}</strong>
                   </div>
               @enderror
-              <input class="uk-input" type="file" id="file" name="radio" class="form-control" accept="audio/*">
+              <input type="file" id="file" name="radio" class="form-control" accept="audio/*">
               <div class="uk-align-right">
-                <button class="uk-button uk-button-primary" type="submit">投稿する</button>
+                <button class="form-button" type="submit">投稿する</button>
               </div>
 
     </form>
